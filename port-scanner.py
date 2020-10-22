72ab9140fd1a57f3aec402742114bebd6f8f2e9f
Hodaifa98 <40869908+Hodaifa98@users.noreply.github.com>
2020-10-22 18:48:05 +0100

    Added threading and main code.

--- port-scanner.py
+++ port-scanner.py
@@ -26,3 +26,27 @@ def fill_queue_with_list(port_list):
     for port in port_list:
         queue.put(port)
 
+def worker():
+    while not queue.empty():
+        port = queue.get()
+        if portScan(port):
+            print(F"Port: {port} is open.")
+            open_ports.append(port)
+
+port_list = range(3290, 3320)
+fill_queue_with_list(port_list)
+
+thread_list = []
+
+for t in range(10):
+    thread = threading.Thread(target=worker)
+    thread_list.append(thread)
+
+for thread in thread_list:
+    thread.start()
+
+for thread in thread_list:
+    #Wait until the thread is done.
+    thread.join()
+
+print("Open ports are: ", open_ports)
