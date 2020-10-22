b08eed9e8ce10387deb3791390137500572eb16a
Hodaifa98 <40869908+Hodaifa98@users.noreply.github.com>
2020-10-22 20:24:15 +0100

    Changed static variables names

--- port-scanner.py
+++ port-scanner.py
@@ -3,17 +3,19 @@ import socket
 import threading
 from queue import Queue
 
-target = "localhost"
+TARGET = "localhost"
+MAX_NUMBER_PORTS = 65535
+NUMBER_OF_THREADS = 500
+
 queue = Queue()
 open_ports = []
-number_of_threads = 500
 
 def portScan(port):
     try:
         #AF_INET = Address family used of IPv4.
         #SOCK_STREAM = TCP instead of UDP.
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target, port))
+        s.connect((TARGET, port))
         return True
     #If a socket couldn't connect, then an exception will be thrown, meaning that the port wasn't open.
     except:
@@ -31,16 +33,16 @@ def worker():
                 port_name = socket.getservbyport(port)
             except:
                 port_name = "UNKNOWN"
-            print(F"Port: {port} is open. Service: {port_name}")
+            print(F"Port: {port} is open. Service: {port_name}.")
             open_ports.append(port)
 
 if __name__ == "__main__":
-    port_list = range(1, 3320)
+    port_list = range(1, 5000)
     fillQueueWithList(port_list)
 
     thread_list = []
 
-    for t in range(number_of_threads):
+    for t in range(NUMBER_OF_THREADS):
         thread = threading.Thread(target=worker)
         thread_list.append(thread)
 
